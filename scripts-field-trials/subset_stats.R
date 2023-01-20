a73c15bf57e400cfab70afc65a28d64426fc8852
Johanna Pedersen <96528149+JohannaPedersen@users.noreply.github.com>
2023-01-20 11:15:59 +0100

    update of statistics datafile + adding new slurry TAN analysis

--- scripts-field-trials/subset_stats.R
+++ scripts-field-trials/subset_stats.R
@@ -1,15 +1,21 @@
 
-# tk = A 
-
+# For LSD analysis:
 statA <- df1[df1$tk == 'A' & df1$elapsed.time == 60 & df1$treat == 'DFC', ]
-
 statB <- df1[df1$tk == 'B' & df1$elapsed.time == 118 & df1$treat == 'DFC', ]
-
-statC <- df1[df1$tk == 'C' & df1$elapsed.time == 119 & df1$treat == 'DFC' & df1$app.meth == 'TH', ]
-
+statC <- df1[df1$tk == 'C' & df1$elapsed.time == 119 & df1$treat == 'DFC', ]
 stat.dat <- rbind(statA, statB, statC)
+stat.dat <- stat.dat[, c('tk', 'id', 'cum.emis.perc', 'app.meth')]
 
-stat.dat <- stat.dat[, c('tk', 'id', 'cum.emis', 'cum.emis.perc')]
+# write.csv(stat.dat, '../data/statLSD_230120.csv')
 
+# For comparing WT and DFC:
+statD <- df1[df1$tk == 'A', ]
+statD <- statD[, c('tk', 'id', 'treat', 'elapsed.time', 'flux.perc')]
 
-write.csv(stat.dat, '../data/stat_230102.csv')
+# adding block info
+statD$block <- statD$id
+statD$block <- mapvalues(statD$block, from = c('1', '6', '10', '15'), to = rep('1', 4))
+statD$block <- mapvalues(statD$block, from = c('3', '7', '12', '16'), to = rep('2', 4))
+statD$block <- mapvalues(statD$block, from = c('5', '9', '13', '17'), to = rep('3', 4))
+
+# write.csv(statD, '../data/statVar_230120.csv')
