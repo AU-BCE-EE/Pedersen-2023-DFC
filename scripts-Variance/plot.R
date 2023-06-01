6febe2971212511cf05dd84e8f91fc5c13a025b8
Johanna Pedersen <96528149+JohannaPedersen@users.noreply.github.com>
2023-06-01 20:08:03 +0200

    update variance plot

--- scripts-Variance/plot.R
+++ scripts-Variance/plot.R
@@ -6,6 +6,6 @@ ggplot(dd, aes(Mean, Variance, color = Treat)) +
   theme_bw() + 
   scale_color_brewer(palette = 'Set1') + 
   ylab('Variance') +
-  xlab('Mean') + 
+  xlab(expression(paste('Mean TAN (%  ',  min^-1,')'))) + 
   theme(legend.position = 'bottom', legend.title = element_blank())
-ggsave2x('../plots-variance/Variance', height = 4, width = 7) 
+ggsave2x('../plots-variance/Variance', height = 3, width = 4) 
